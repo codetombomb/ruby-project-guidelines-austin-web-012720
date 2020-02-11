5dc24a5800f947bee29cb2aaaca30abcb9ec240a
github username <github email address>
2020-02-10 18:06:44 -0600

    Added Seed file

---
+++ db/seed.rb
@@ -0,0 +1,7 @@
+maylene = User.new(name: "Maylene")
+tom = User.new(name: "Tom")
+mark = USer.new(name: "Mark")
+
+schools = Category.new(category_name: "Schools")
+sports = Category.new(category_name: "Sports")
+bbq = Category.new(category_name: "Best BBQ")
--- lib/user.rb
+++ lib/user.rb
@@ -2,5 +2,15 @@ class User < ActiveRecord::Base
     has_many :user_categories
     has_many :categories, through: :user_categories  
     
-    puts "Test"
+    def create_user
+        #Ask user to enter a name and a password 
+        #check to see that it is not already in the database
+        #
+    end
+
+    def find_user
+        #Prompts user to enter their username and verifies password
+    end
+
+
 end
