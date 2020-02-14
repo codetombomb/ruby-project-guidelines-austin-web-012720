84ed18dbf86589b35bf94cca0066d72243758bed
github username <github email address>
2020-02-13 18:01:54 -0600

    added e in CreateUserQuestion

---
+++ db/migrate/20200212152515_create_user_questions.rb
@@ -0,0 +1,8 @@
+class CreateUserQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_questions do |t|
+      t.integer :user_id
+      t.integer :question_id
+    end
+  end
+end
